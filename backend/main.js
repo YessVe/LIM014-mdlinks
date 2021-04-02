821b1d0879b0c94d34cbfeb509d4b5854b5a8b14
Yessenia <vela.kathy@gmail.com>
2021-04-01 19:11:58 -0500

    añade una funcón que engloba existencia de ruta y si es absoluta como borrador

--- backend/main.js
+++ backend/main.js
@@ -1,4 +1,8 @@
-/*const marked = require('marked');
+/* module.exports = () => {
+  // ...
+}; */
+
+/*
 const fetch = require('node-fetch');
 const chalk = require('chalk');  
 path2 = pathNode.normalize(path); // will try and calculate the actual path, when it contains relative specifiers like . or .., or double slashes 
@@ -8,20 +12,55 @@ fs.readdir() // o read the contents of a directory. This piece of code reads the
 
 const pathNode = require('path');
 const fs = require('fs');
-path = process.argv[2]; //the command line arguments are always stored in an array. The second element is the javascript (JS) file we refer to that often comes after the node command.
+const marked = require('marked');
+/* path = process.argv[2]; //the command line arguments are always stored in an array. The second element is the javascript (JS) file we refer to that often comes after the node command.
 option = process.argv[3];
 option2 = process.argv[4];
-console.log(option);
-console.log(option2);
+console.log(path); */
+/* console.log(option2); */
+
+/* path1 = pathNode.resolve(path); //get the absolute path calculation of a relative path
+console.log(path1);
+fileExists = fs.existsSync(path1);
+console.log(fileExists); */
 
 //FUNCIÓN PARA VALIDAR SI LA RUTA ES ABSOLUTA
 //FUNCIÓN PARA LAS OPCIONES 1) --validate, 2) --stats y 3) --stats --validate
+
+//FX VALIDAR SI LA RUTA ES ABSOLUTA Y EXISTE
+/* let ruta = '../practica' */
+let ruta = 'D:\\GitHub\\LIM014-mdlinks\\practica\\ARCH SIN LINKS.md'
+
+//FX QUE VALIDA LA RUTA Y VE SI EXISTE O NO
+function rutaExist (ruta) {  
+
+    if (pathNode.isAbsolute(ruta) === true) {
+        if (fs.existsSync(ruta) === true) {
+            console.log('ruta ABSOLUTA y SÍ EXISTE');
+            return ruta;
+        } else {
+            return console.log('ruta ABSOLUTA y NO EXISTE');
+        }
+    } else {
+        path1 = pathNode.resolve(__dirname,ruta);
+        if (fs.existsSync(path1) === true) {
+            console.log('ruta  NO ABSOLUTA y SÍ EXISTE');
+            return path1
+        } else {
+            return console.log('ruta NO ABSOLUTA y NO EXISTE');
+        }
+    }
+}
+
+rutaExist(ruta);
+
 if (path.indexOf("\\") == -1) { //si no lo encuentra, es el nombre del archivo  
      path1 = pathNode.resolve(path); //get the absolute path calculation of a relative path
      fileExists = fs.existsSync(path1);
     if (fileExists === true && path1.slice(-3) === ".md") {   
         console.log('analizar archivo');
         console.log('es un archivo .MD');
+        console.log(fileExists);
 
         if (option === "--validate") {
             console.log("CLI validate");
@@ -46,5 +85,27 @@ if (path.indexOf("\\") == -1) { //si no lo encuentra, es el nombre del archivo
 }
 
 
+
+//FX QUE VALIDA LA RUTA Y VE SI EXISTE O NO
+/* function rutaExist (ruta) {  
+    if (pathNode.isAbsolute(ruta) === true) {
+        if (fs.existsSync(ruta) === true) {
+            console.log('ruta ABSOLUTA y SÍ EXISTE');
+            return ruta;
+        } else {
+            return console.log('ruta ABSOLUTA y NO EXISTE');
+        }
+    } else {
+        path1 = pathNode.resolve(__dirname,ruta);
+        if (fs.existsSync(path1) === true) {
+            console.log('ruta  NO ABSOLUTA y SÍ EXISTE');
+            return path1
+        } else {
+            return console.log('ruta NO ABSOLUTA y NO EXISTE');
+        }
+    }
+}
+
+console.log(rutaExist(ruta1)); */
 
 
